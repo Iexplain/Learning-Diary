72989c3b412d1b6a6283a96011c72a06a3b6dc95
Iexplain <94895474+Iexplain@users.noreply.github.com>
2026-03-20 17:35:45 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // 关键：生成纯静态 HTML/CSS/JS
+  output: 'export', 
   images: {
     unoptimized: true,
   },
+  // 关键新增：告诉 Next.js 你的 GitHub 仓库名作为基础路径
+  basePath: '/Learning-Diary', 
 };
 export default nextConfig;
